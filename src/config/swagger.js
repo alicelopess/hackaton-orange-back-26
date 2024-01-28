4d403125b4d67bea76bf014f728fac19448042bf
Pedro <pedrohenriquemartins@live.com>
2024-01-28 04:52:00 -0300

    add configura swagger para as rotas de projetos

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,18 +1,150 @@
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API"
+  "definition": {
+    "openapi": "3.0.0",
+    "info": {
+      "title": "Orange portfolio",
+      "description": "Portfolio de projetos",
+      "version": "1.0.0",
     },
-    servers: [
-      {
-        url: "http://localhost:3333"
+    "paths": {
+        "/projects": {
+          "get": {
+            "summary": "Retorne um array de projetos",
+            "description": "Retorna um array com todos os projetos criados",
+            "tags": ['Projects'],
+            "responses": {
+              "200": {
+                "description": "OK"
+              }
+            }
+          },
+          "post": {
+            "summary": "Cria um projeto",
+            "description": "Cria um novo projeto",
+            "tags": ["Projects"],
+            "requestBody": {
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "type": "object",
+                    "properties": {
+                      "title": {
+                        "type": "string"
+                      },
+                      "tag": {
+                        "type": "string"
+                      },
+                      "link": {
+                        "type": "string"
+                      },
+                      "description": {
+                        "type": "string"
+                      },
+                      "image": {
+                        "type": "string",
+                        "format": "binary"
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            "responses": {
+              "201": {
+                "description": "Projeto Criado!",
+                "content": {
+                  "application/json": {
+                    "schema": {
+                      "type": "object",
+                    }
+                  }
+                }
+              }
+            }
+          },
+        },
+        "/projects/{id}": {
+          "put": {
+            "summary": "Altera um projeto",
+            "description": "Altera um projeto de acordo com o ID",
+            "tags": ["Projects"],
+            "parameters": [
+              {
+                "name": "id",
+                "in": "path",
+                "description": "ID do projeto que será atualizado",
+                "required": true,
+              },
+            ],
+            "requestBody": {
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "type": "object",
+                    "properties": {
+                      "title": {
+                        "type": "string"
+                      },
+                      "tag": {
+                        "type": "string"
+                      },
+                      "link": {
+                        "type": "string"
+                      },
+                      "description": {
+                        "type": "string"
+                      },
+                      "image": {
+                        "type": "string",
+                        "format": "binary"
+                      }
+                    }
+                  }
+                }
+              }
+            },
+            "responses": {
+              "203": {
+                "description": "Projeto Atualizado!",
+                "content": {
+                  "application/json": {
+                    "schema": {
+                      "type": "object",
+                    }
+                  }
+                }
+              }
+            }
+          },
+          "delete": {
+            "summary": "Deleta um projeto",
+            "description": "Deleta um projeto de acordo com o ID",
+            "tags": ["Projects"],
+            "parameters": [
+              {
+                "name": "id",
+                "in": "path",
+                "description": "ID do projeto que será deletado",
+                "required": true,
+              },
+            ],
+            "responses": {
+              "203": {
+                "description": "Projeto Deletado!",
+                "content": {
+                  "application/json": {
+                    "schema": {
+                      "type": "object",
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
       }
-    ],
-  },
-  apis: ["./routes/*.js"]
+    },
+  "apis": ["../routes/router.js"]
 }
 
 export default options
