f864dc6acabe0555556fc01e28730bcd42471d67
Maria Alice <alicedflopes.02@gmail.com>
2024-02-02 22:07:04 -0300

    wip: conexao com o banco de dados do mongo

--- src/config/app.js
+++ src/config/app.js
@@ -4,6 +4,7 @@ import swaggerUI from 'swagger-ui-express'
 import swaggerJsDoc from 'swagger-jsdoc'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import connection from '../models/connection.js'
 
 
 import options from './swagger.js'
@@ -14,6 +15,9 @@ const app = express()
 
 app.use(cors()) //habilitando cors na aplicação
 app.use(bodyParser.urlencoded({extended: true}))
+
+connection
+
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 app.use(express.json())
 app.use(router)
